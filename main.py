0599a6f8250db89a5423d02695517798548b9835
theo laptop <theo.badoz@gmail.com>
2023-05-24 10:06:44 +0200

    create a tab3.py file for greater clarity

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from utils import get_challenge_output, send_simple_message
 from tab2 import fine_tuning, few_shot_learning
+from tab3 import test_form, challenge_form
 
 # SETTING PAGE CONFIG TO WIDE MODE
 st.set_page_config(page_icon="⚙️", page_title="L'IA c'est pas soricer : Challenge FSL", layout="wide")
@@ -25,27 +25,8 @@ def main_tab2():
 
 def main_tab3():
     st.header("L'analyse de sentiment")
-    with st.form(key='prompt FSL'):
-        st.markdown('Récupérez votre token HuggingFace ici: https://huggingface.co/settings/tokens')
-        API_TOKEN = st.text_input(label="HF_API_TOKEN")
-        HEADERS = {"Authorization": f"Bearer {API_TOKEN}"}
-        API_URL = "https://api-inference.huggingface.co/models/EleutherAI/gpt-neo-2.7B"
-        prompt = st.text_area(label='Your prompt', value='''Task to perform\nexample1\nsol1\n###\nexample2\nsol2\n###\nexample3\nsol3\n###\ntest''')
-        parameters = {
-            "max_new_tokens": 1,
-            "temperature": .5,
-            "end_sequence": "###"
-            }
-        submit_FSL_test = st.form_submit_button(label='Test this setup')
-    if submit_FSL_test:
-        st.markdown(get_challenge_output(API_URL, HEADERS, prompt, parameters))
-    with st.form(key='Submit your model to the challenge', clear_on_submit=True):
-        contact_pseudo = st.text_input(label="Pseudo")
-        contact_mail = st.text_input(label="Email")
-        submitted_model = st.text_area(label="Copy paste your prompt here")
-        submit_FSL_model = st.form_submit_button(label='Soumettre ma participation au challenge !')
-    if submit_FSL_model:
-        send_simple_message(contact_pseudo, contact_mail, submitted_model)
+    test_form()
+    challenge_form()
 
 tab1, tab2, tab3 = st.tabs(["LLM qu'est-ce que c'est", "LLM comment s'en servir", "Le challenge"])
 with tab1:
---
+++ tab3.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from utils import get_challenge_output, send_simple_message
+
+
+def test_form():
+    with st.form(key='prompt FSL'):
+        st.markdown('Récupérez votre token HuggingFace ici: https://huggingface.co/settings/tokens')
+        API_TOKEN = st.text_input(label="HF_API_TOKEN")
+        HEADERS = {"Authorization": f"Bearer {API_TOKEN}"}
+        API_URL = "https://api-inference.huggingface.co/models/EleutherAI/gpt-neo-2.7B"
+        prompt = st.text_area(label='Your prompt', value='''Task to perform\nexample1\nsol1\n###\nexample2\nsol2\n###\nexample3\nsol3\n###\ntest''')
+        parameters = {
+            "max_new_tokens": 1,
+            "temperature": .5,
+            "end_sequence": "###"
+            }
+        submit_FSL_test = st.form_submit_button(label='Test this setup')
+    if submit_FSL_test:
+        st.markdown(get_challenge_output(API_URL, HEADERS, prompt, parameters))
+
+
+def challenge_form():
+    with st.form(key='Submit your model to the challenge', clear_on_submit=True):
+        contact_pseudo = st.text_input(label="Pseudo")
+        contact_mail = st.text_input(label="Email")
+        submitted_model = st.text_area(label="Copy paste your prompt here")
+        submit_FSL_model = st.form_submit_button(label='Soumettre ma participation au challenge !')
+    if submit_FSL_model:
+        send_simple_message(contact_pseudo, contact_mail, submitted_model)
